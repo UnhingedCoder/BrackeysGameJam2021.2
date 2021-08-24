8c8b57bdec3a990d7d371283d580a0f0fbb2bf89
Chris Simon <chris22simon@gmail.com>
2021-08-24 19:51:26 +0530

    Bullet prefab

--- BrackeysGameJam/Assets/Scripts/Projectiles.cs
+++ BrackeysGameJam/Assets/Scripts/Projectiles.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IProjectile
-{
-  //  float speed 
-}
-
 public class Projectiles : MonoBehaviour
 {
-   // [SerializeField] protected
+    [SerializeField] protected float Speed;
+    [SerializeField] protected int Damage;
+    [SerializeField] protected int Bounces;
+
+    Rigidbody m_rigidbody;
+
+    private void Start()
+    {
+        m_rigidbody = this.gameObject.GetComponent<Rigidbody>();
+
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.LogError("SPACE");
+            m_rigidbody.AddForce(transform.forward * Speed);
+        }
+
+       // this.transform.Translate(Vector3.one.normalized * Speed * Time.deltaTime);
+    }
+
 }
