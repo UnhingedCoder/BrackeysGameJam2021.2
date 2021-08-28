cb71ef0cc7d97f9a4e1528064e69ad987391f395
UnhingedCoder <ayan.chkbrty@gmail.com>
2021-08-28 14:51:00 +0530

    Player health bar gradient

--- BrackeysGameJam/Assets/Scripts/Player/PlayerHealthView.cs
+++ BrackeysGameJam/Assets/Scripts/Player/PlayerHealthView.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class PlayerHealthView : MonoBehaviour
 {
     #region VARIABLE_REG
+    [SerializeField] private GameObject m_healthContainer;
     [SerializeField] private Image m_healthBar;
     [SerializeField] private PlayerController m_pController;
+    [SerializeField] private Gradient m_healthBarGradient;
+
+    [SerializeField] private ViewsManager m_viewsManager;
     #endregion
 
     #region UNITY_REG
+    private void Awake()
+    {
+        m_viewsManager = FindObjectOfType<ViewsManager>();    
+    }
+
     private void OnEnable()
     {
         m_pController = this.transform.root.GetComponent<PlayerController>();
@@ -26,7 +35,11 @@ public class PlayerHealthView : MonoBehaviour
 
     void UpdateHealth()
     {
+        bool state = m_viewsManager.currentGameState == GameState.InGame ? true : false;
+        m_healthContainer.SetActive(state);
+
         m_healthBar.fillAmount = m_pController.CurrentHealthPercentage();
+        m_healthBar.color = m_healthBarGradient.Evaluate(m_pController.CurrentHealthPercentage());
     }
     #endregion
 }
